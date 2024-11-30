219d85a8656d328dc23cf9a50a1cec15e1d1514d
Muhammad Suleman <tech4you330@gmail.com>
2024-12-01 04:08:05 +0500

    Logic Building Part 1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,32 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.model.js" 
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-      message: "Ok"
+    // get user details from frontend  
+    // validation -- not empty  
+    // check if user already exist: username or email  
+    // check for images, check for avatar  
+    // upload them to cloudinary, avatar  
+    // create user object - create entry in db  
+    // remove password and refresh token field from response  
+    // check for user creation
+    // return response  
+
+    const {userName, fullName, password, email} = req.body;
+    console.log("email, ", email);
+
+    if([fullName, email, userName, password].some((fields) => fields?.trim() === "")){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{userName}, {email}]
     })
+
+    if(existedUser){
+        throw new ApiError(409, "User with email or username already exists")
+    }
 })
 
 export {registerUser}
